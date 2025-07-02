d7a067fcd90b3ca678009165232bd3a1a88f2147
threat-feAE <kopadzetemuri9@gmail.com>
2025-07-02 14:02:07 +0400

    1

---
+++ day01/classwork/cw.py
@@ -0,0 +1,7 @@
+name = "Temo"         # String ტიპის ცვლადი
+age = 18              # Integer ტიპის ცვლადი
+height = 1.75         # Float ტიპის ცვლადი
+
+print("სახელი:", name)
+print("ასაკი:", age)
+print("სიმაღლე:", height)
