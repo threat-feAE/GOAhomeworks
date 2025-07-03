77c8ed5855adcdbb94a470a75be0b61ea975b2ec
threat-feAE <kopadzetemuri9@gmail.com>
2025-07-03 14:53:46 +0400

    1

---
+++ day01/homework/cw.py
@@ -0,0 +1,11 @@
+print("chven viswavlet 'string' 'integer' 'foat' 'boolean' da 'print'")
+print("da dges me avxsni tu ra punkcias asrulebs titoeuli matgani")
+print("modi daviwyot 'string'.'string' aris teksti romelic gvakvs brwyalebshi mocemuli")
+print("shemdegi aris 'integer'.'integer' aris mteli ricxvebi magalitad:15;29;58")
+print("exla visaubrot 'float','float' aris atwiladi ricxvebi magalitad:1.3;2,9;4,1 da ase shemdeg")
+print("shemdeg 'boolean/bulioni'.'boolean/bulioni' akvs  ori mnishvneloba 'true' da 'false' magalitad:")
+print ("is_sunny = True""is_raining = False")
+is_sunny = True
+is_raining = False
+print("da bolo aris 'print'.'print' aris punkcia romelsac gamoakvs shedegi terminalze.magalitad 'print(koapdze) ")
+print("da sul es iyo.davaleba shesrulebulia")
