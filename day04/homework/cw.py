ddd515e059c862feaa4266c24592579f351ae4b8
threat-feAE <kopadzetemuri9@gmail.com>
2025-07-15 19:07:57 +0400

    1

---
+++ day04/homework/cw.py
@@ -0,0 +1,62 @@
+print("input punkcia gamoiyeneba momxmareblisgan monacemebis misgebad")
+print("xolo output nishnavs roca programa mushaobs da unda dabewdos shedegi ekranze")
+
+
+name=("temo")
+print(name)
+
+
+
+
+num1=int(222)
+num2=int(1234)
+print(num1 + num2)
+
+
+
+name=("temo.")
+age=("15.")
+city=("samtredia.")
+weight=("58,2.")
+print(name + age + city + weight)
+
+
+
+
+name=("temo.")
+age=("15.")
+city=("samtredia.")
+weight=("58,2.")
+print(name, age, city, weight)
+
+
+
+word1=("GOA")
+word2=("BEST")
+word3=("ACADEMY")
+print(word1, word2, word3)
+
+
+
+name=("temo")
+surname=("kopadze")
+age=(15)
+city=("samtredia")
+print("i am", name, surname + ",",age)
+
+
+
+
+a=(23)
+b=(25)
+c=(45)
+d=(10)
+e=(62)
+print(a, b, c, d, e)
+
+
+
+
+age=(15)
+if age<18:print("underage!")
+else:print("adult!")
