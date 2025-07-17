fb37c6f8246cdac016b989564dde05e0c1d77779
threat-feAE <kopadzetemuri9@gmail.com>
2025-07-17 16:14:15 +0400

    1

--- day05/classwork/cw.py
+++ day05/classwork/cw.py
@@ -1,5 +1,5 @@
-num1= int(input(23))
-num2= int(input(235))
+num1 = int(input("enter first number"))
+num2 = int(input("enter second number"))
 print(num1 > num2)
 print(num1 < num2)
 print(num1 == num2)
