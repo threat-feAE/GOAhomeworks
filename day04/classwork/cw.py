83c9b0751e77221354bf7d56e6b62bc1f6b0fc8b
threat-feAE <kopadzetemuri9@gmail.com>
2025-07-17 15:55:26 +0400

    1

---
+++ day04/classwork/cw.py
@@ -0,0 +1,27 @@
+name = input("შეიყვანე შენი სახელი: ")
+print("შენი სახელი არის:", name)
+
+
+
+num1 = int(input("შეიყვანე პირველი რიცხვი: "))
+num2 = int(input("შეიყვანე მეორე რიცხვი: "))
+sum_result = num1 + num2
+print("ორ რიცხვს შორის ჯამი არის:", sum_result)
+
+
+
+age = int(input("შეიყვანე შენი ასაკი: "))
+print("შენი ასაკის მონაცემთა ტიპია:", type(age))
+
+
+
+name = "თემო"         # string
+age = 18              # integer
+height = 1.75         # float
+is_student = True     # boolean
+
+print(type(name))       # <class 'str'>
+print(type(age))        # <class 'int'>
+print(type(height))     # <class 'float'>
+print(type(is_student)) # <class 'bool'>
+
