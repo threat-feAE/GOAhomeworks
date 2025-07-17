613d202f4dcf8e1cd526ab18ef25a98cd03b1649
threat-feAE <kopadzetemuri9@gmail.com>
2025-07-17 15:52:40 +0400

    1

--- day04/homework/cw.py
+++ day04/homework/cw.py
@@ -1,8 +1,8 @@
-print("input punkcia gamoiyeneba momxmareblisgan monacemebis misgebad")
-print("xolo output nishnavs roca programa mushaobs da unda dabewdos shedegi ekranze")
+#input punkcia gamoiyeneba momxmareblisgan monacemebis misgebad
+#xolo output nishnavs roca programa mushaobs da unda dabewdos shedegi ekranze
 
 
-name=("temo")
+name=("temo")          #string
 print(name)
 
 
@@ -14,49 +14,49 @@ print(num1 + num2)
 
 
 
-name=("temo.")
-age=("15.")
-city=("samtredia.")
-weight=("58,2.")
+name=("temo.")          #string
+age=("15.")             #string     #integer
+city=("samtredia.")     #string     
+weight=("58,2.")        #string
 print(name + age + city + weight)
 
 
 
 
-name=("temo.")
-age=("15.")
-city=("samtredia.")
-weight=("58,2.")
+name=("temo.")         #string
+age=("15.")            #string       #integer
+city=("samtredia.")    #string
+weight=("58,2.")       #string     
 print(name, age, city, weight)
 
 
 
-word1=("GOA")
-word2=("BEST")
-word3=("ACADEMY")
+word1=("GOA")          #string
+word2=("BEST")         #string
+word3=("ACADEMY")      #string
 print(word1, word2, word3)
 
 
 
-name=("temo")
-surname=("kopadze")
-age=(15)
-city=("samtredia")
+name=("temo")          #string
+surname=("kopadze")    #string
+age=(15)               #integer 
+city=("samtredia")     #string
 print("i am", name, surname + ",",age)
 
 
 
 
-a=(23)
-b=(25)
-c=(45)
-d=(10)
+a=(23)    #integer
+b=(25)    #integer
+c=(45)    #integer
+d=(10)    #integer
 e=(62)
 print(a, b, c, d, e)
 
 
 
 
-age=(15)
-if age<18:print("underage!")
-else:print("adult!")
+age=(15)                                #integer
+if age<18:print("underage!")             #string
+else:print("adult!")                     #string
---
+++ day05/classwork/cw.py
@@ -0,0 +1,5 @@
+num1= int(input(23))
+num2= int(input(235))
+print(num1 > num2)
+print(num1 < num2)
+print(num1 == num2)
