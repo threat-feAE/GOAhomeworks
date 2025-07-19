e2000c42b8385a89da2f074a80aa9b04e9afa807
threat-feAE <kopadzetemuri9@gmail.com>
2025-07-19 19:35:54 +0400

    1

---
+++ day02/homework/cw.py
@@ -0,0 +1,59 @@
+a=35
+b=45
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+
+
+#string
+Name="temo"
+city="samtredia"
+color="blue"
+fruit="ananas"
+
+#integer
+age=32
+year=2345
+price=450
+score=5
+
+
+#float
+height=1.75
+weight=45.7
+speed=176.01
+size=16.24
+
+
+#gamotana
+
+print(Name, type(Name))
+print(age, type(age))
+print(weight,type(weight))
+
+
+
+name=("temo")
+height=("tall")
+massage=(name + "is" + height)
+print(massage)
+
+
+#strings("sky" "candy" "light" "school" "apple")
+#integers(1,2,5,99,3423)
+#floats(1.12,23.3,2.34,1.4556,9.54)
+
+a=("sky")
+b=("shy")
+c=("why")
+d=("buy")
+e=("cry")
+print(a + b + c + d + e)
+
+a=(23)
+b=(2.3)
+c=(a + b)
+d=(a * b)
+e=(a - b)
+print(c,d,e)
---
+++ day05/homework/cw.py
@@ -0,0 +1,41 @@
+#case sensitive ნიშნავს რომ სისტემა განასხვავებს დიდ და პატარა ასოებს
+#snake case ეს არის წერილის სტილი, რომელსაც ხშირად იყენებენ პრიგრამირებაში ცვლადების ან პუნკციების დასაწერად
+#კიდევ ვისწავლეთ true და false
+
+
+num1=(1225)
+num2=(5032)
+print(num1>num2)
+print(num1<num2)
+print(num1==num2)
+
+
+a=("goon")
+b=("choped")
+c=("sigma")
+d=("mog")
+print(a + "," + b)
+print(c + "," + d)
+
+
+
+a=34
+b=56
+c=1
+d=980
+print((a + b + c + d)/4)
+
+
+
+
+num1=("blue")
+num2=("green")
+print(num1==num2)
+
+
+
+print(int("40"))
+print(int("0"))
+print(int("6780"))
+print(int("345"))
+
