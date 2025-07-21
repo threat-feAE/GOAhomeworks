7212c7295eb7452b16c8101c551a015ea4ed5761
threat-feAE <kopadzetemuri9@gmail.com>
2025-07-21 22:53:50 +0400

    1

---
+++ day06/classwork/cw.py
@@ -0,0 +1,31 @@
+# ლოგიკური ოპერატორები
+#—————————————————————
+#.and-ოპერატორის დროს თუ ერთი პირობა მცდარი იყო მაშინ ყველა მცდარი იქნება . 
+
+#.or-ოპერატორის დროს თუ ერთი პირობა მაინც იყო სწორი ყველა სწორი იქნება . 
+
+#true and true—>true           
+#true or true—>true
+#true and false—>false
+#false or true—>true
+#false and false_>false
+#true or false—>ture
+#false and true—>false
+#false or false_>false
+
+
+
+num1=("koba")
+num2=("temo")
+print(num1==num2)
+
+
+print(True and False)
+print(True and True)
+
+
+print(False or False)
+print(False or True)
+
+
+
---
+++ day06/homework/cw.py
@@ -0,0 +1,26 @@
+num1=(2332436)
+num2=(3491234)
+
+
+print(num1<num2)
+print(num1>num2)
+print(num1==num2)
+
+
+n=(int("23"))
+p=(int("523"))
+q=(int("12"))
+w=(67)
+k=(89)
+
+print(int(n*p*q*w*k)/5)
+
+
+#   (and)                             (or)
+#True and True -- ... true     |   True or True -- ... true         
+#True and False -- ... false    |   True or False -- ...ture
+#False and True -- ... false    |   False or True -- ...true
+#False and False -- ... false   |   False or False -- ... false
+
+
+
