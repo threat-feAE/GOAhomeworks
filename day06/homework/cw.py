55ced50a3a57b52559f7808940451ff40b556d0c
threat-feAE <kopadzetemuri9@gmail.com>
2025-07-22 10:33:03 +0400

    1

--- day06/homework/cw.py
+++ day06/homework/cw.py
@@ -1,3 +1,22 @@
+# ლოგიკური ოპერატორები
+#—————————————————————
+#.and-ოპერატორის დროს თუ ერთი პირობა მცდარი იყო მაშინ ყველა მცდარი იქნება . 
+
+#.or-ოპერატორის დროს თუ ერთი პირობა მაინც იყო სწორი ყველა სწორი იქნება . 
+
+#true and true—>true           
+#true or true—>true
+#true and false—>false
+#false or true—>true
+#false and false_>false
+#true or false—>ture
+#false and true—>false
+#false or false_>false
+
+
+
+
+
 num1=(2332436)
 num2=(3491234)
 
@@ -5,6 +24,9 @@ num2=(3491234)
 print(num1<num2)
 print(num1>num2)
 print(num1==num2)
+
+
+
 
 
 n=(int("23"))
@@ -16,6 +38,10 @@ k=(89)
 print(int(n*p*q*w*k)/5)
 
 
+
+
+
+
 #   (and)                             (or)
 #True and True -- ... true     |   True or True -- ... true         
 #True and False -- ... false    |   True or False -- ...ture
@@ -23,4 +49,63 @@ print(int(n*p*q*w*k)/5)
 #False and False -- ... false   |   False or False -- ... false
 
 
+
+print(True and True)
+print(False and True)
+print(True and False)
+print(False and False)
+
+print(True or False)
+print(False or False)
+print(False or True)
+print(True or True)
+
+
+x="hello"   #string
+y=10        #integer
+z=3.5       #float
+t=True      #boolean
+
+
+print(x)
+print(y)
+print(z)
+print(t)
+
+
+
+a=(3.5)
+b=(23)
+c=("kopa tyviaa")
+
+print(a)
+print(b)
+print(c)
+
+
+
+
+
+num1=12390812489023450725
+num2=56790234578965123844
+
+print(num1>num2)
+print(num1<num2)
+print(num1==num2)
+
+
+name="kopalopagopatropa"
+print(name)
+
+
+
+
+
+
+
+
+
+x=234
+y=2444
+print((x*y)/2)
 
