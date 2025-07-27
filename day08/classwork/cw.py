5d525ba7eb998d5c046002974785d31021b5751c
threat-feAE <kopadzetemuri9@gmail.com>
2025-07-27 13:39:34 +0400

    1

---
+++ day08/classwork/cw.py
@@ -0,0 +1,37 @@
+r=234
+q=567
+h=111
+g=756
+f=560
+l=230
+
+
+print(r>l)
+print(f>h)
+print(g>f)
+
+print(g<f)
+print(f<l)
+print(q<r)
+
+
+
+print(7<10 or 4==9)
+print(5>3 and 10>2)
+print(12445<312546 or 2>435)
+
+
+
+print(73<10 or 4==9)
+print(5>36 and 10>2)
+print(12447775<312546 or 2>435)
+
+
+
+
+
+
+
+
+
+for i in range(20):print("temo")
