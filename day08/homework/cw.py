20d0d6f1d898cf31a6c4031f138cfd3bd75e9aff
threat-feAE <kopadzetemuri9@gmail.com>
2025-07-29 14:53:07 +0400

    1

---
+++ day08/homework/cw.py
@@ -0,0 +1,93 @@
+print(7<10 or 4==9)
+print(5>3 and 10>2)
+print(12445<312546 or 2>435)
+
+
+
+print(73<10 or 4==9)
+print(5>36 and 10>2)
+print(12447775<312546 or 2>435)
+
+
+
+print(7<107 or 4==567)
+print(5342>35 and 10>5552)
+print(12445<312546 or 2>435)
+
+
+
+print(657563<10 or 4==9675675)
+print(5>5675636 and 10>7672)
+print(175<312546 or 675672>435)
+
+
+
+
+#Sequencing = ინსტრუქციების რიგრიგობით შესრულება, ზუსტად იმ წესით, როგორც კოდში წერია.     Iteration ნიშნავს, რომ კოდი ან ინსტრუქციები მეორდება რამდენჯერმე, სანამ დათქმული პირობა სრულდება ან სანამ სია არ დამთავრდება.       Selection ნიშნავს, რომ პროგრამა ირჩევს სხვადასხვა გზას ან მოქმედებას, პირობის მიხედვით. ანუ თუ რაღაც პირობა მართალია, გააკეთე ერთი რამ; თუ არა — სხვა.
+
+
+
+
+
+name=input("kopa")
+greetings="hello" + name
+print(greetings)
+
+#sepuencing imitom aris rom rigrigobit asrulebs instrukcias
+
+
+# loop aris cikli rac ameorebs kods magalitad 100jerac
+
+for i in range(25):print("kopa")
+
+for i in range(20):print("BMW")
+
+
+#range() aris punkcia romelsac viyenebt for loopshi ricxvebis seriis shesakmnelad
+
+range(2)
+
+
+
+
+for i in range(100):print("kopadze")
+for i in range(46):print("black")
+for i in range(32):print("T")
+
+
+
+# მომხმარებლისგან შემოტანა
+str1 = input("kopa")
+str2 = input("lopa")
+str3 = input("tropa")
+num1 = str(input("14"))
+
+# კონკატინაცია (რიცხვი გადაქცეულია სტრინგად str() ფუნქციით)
+result = str1 + " " + str2 + " " + str3 + " " + num1
+
+# შედეგის გამოყვანა
+print(result)
+
+
+
+
+name = "თემო"          # სტრინგი
+age = 17               # ინტეჯერი
+height = 1.75          # ფლოატი
+is_student = True      # ბულიანი
+
+# მონაცემთა ტიპების გამოტანა
+print(type(name))        # <class 'str'>
+print(type(age))         # <class 'int'>
+print(type(height))      # <class 'float'>
+print(type(is_student))  # <class 'bool'>
+
+
+
+
+
+k=2334
+c=456
+d=4321
+b=896
+print(k+c+d+b)
