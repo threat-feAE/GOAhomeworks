a9205ec2a3a8de87ec907afe6b0f8a683004cdb6
threat-feAE <kopadzetemuri9@gmail.com>
2025-08-05 13:03:30 +0400

    1

---
+++ day10/homework/cw.py
@@ -0,0 +1,116 @@
+for i in range(0,39):
+    print(i)
+
+
+
+
+
+for i in range(30,40):
+    print(i)
+
+
+
+
+
+for i in range(12,120):
+    print(i)
+
+
+
+
+for i in range(18):
+    print("temo,kopadze")
+
+
+
+
+
+for i in range(10,100,3):
+    print(i)
+
+
+
+
+for i in range(1,31):
+    print(i, "banani")
+
+
+
+
+for i in range(1,31):
+    print(str(i) + "vashli")
+
+
+
+
+
+
+for i in range(13):
+    print(33)
+
+
+
+
+
+for i in range(1,31):
+    input("banani")
+
+
+
+
+
+
+for i in range(0,123,4):
+    print(i)
+    input("kopa")
+
+
+
+
+
+for i in range(0,123,):
+    name=input("kopa")
+    print("temo" + str(i) + name)
+
+
+
+
+
+    print(39>123 or 23==23)
+
+
+
+
+
+i=0
+while i<10:
+    print("temo")
+    i=i+1
+
+
+
+
+i=0
+while i<41:
+    print(i)
+    i=i+1
+
+
+
+
+i=0
+while i<111:
+    print("kopaa")
+    i=i+12
+    
+
+
+
+
+
+for i in range(0,1234,231):
+    name=input("kopa")
+    print("lopaaq" + str(i) + name)
+
+
+    
