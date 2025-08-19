c0d337ede99e069b8eeb5efee9768fb688447556
threat-feAE <kopadzetemuri9@gmail.com>
2025-08-19 14:21:28 +0400

    1

---
+++ day12/homework/cw.py
@@ -0,0 +1,24 @@
+# Conditional Statement (პირობითი განცხადება) საშუალებას გვაძლევს
+# პროგრამამ მიიღოს გადაწყვეტილება რაღაც პირობის მიხედვით.
+# დანიშნულება: თუ პირობა მართალია -> ერთი ბლოკი შესრულდეს, თუ არა -> სხვა.
+# სახეები: if, if-else, if-elif-else
+
+
+
+for i in range(50):
+    print("hello world")
+
+
+
+x = 3
+while x <= 17:
+    print(x)
+    x += 1
+
+
+
+password = input("შეიყვანე პაროლი: ")
+if password == "1234":
+    print("Password is correct")
+else:
+    print("Password is incorrect")
