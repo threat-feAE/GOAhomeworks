273ceed3b2db074c77b06b735e49c28548c292d3
threat-feAE <kopadzetemuri9@gmail.com>
2025-09-02 19:04:47 +0400

    1

---
+++ day18/homework/cw.py
@@ -0,0 +1,62 @@
+a=("12")
+b=("34")
+
+if a > b:
+    print("pirveli metia meoreze")
+elif b > a:
+    print("pirveli naklebia meoreze")
+elif a == b:
+    print("pirveli tolia meores")
+
+
+
+
+
+user_input = input("შეიყვანე სახელი: ")
+my_name = "Temo"  # აქ ჩაწერე შენი სახელი
+
+if user_input == my_name:
+    print("opaaa sexniaaaa")
+else:
+    print("sxvadasxva saxelebi gvakvs")
+
+
+
+
+
+
+# ორი ინტეჯერი
+num1 = int(input(""))
+num2 = int(input(""))
+
+if num1 > 0 and num2 > 0:
+    print("ორივე რიცხვი დადებითია")
+elif num1 < 0 and num2 < 0:
+    print("ორივე რიცხვი არის უარყოფითი")
+else:
+    print("ეს რა ჯანდაბაა")
+
+
+
+
+
+
+
+
+
+for i in range (50,101):
+    print(i)
+
+
+
+
+
+
+
+
+
+
+i = 20
+while i < 40:
+    print(i)
+    i +=1
