a81434c214d185fe3326443d53cf37d21c048756
threat-feAE <kopadzetemuri9@gmail.com>
2025-09-13 22:52:45 +0400

    1

---
+++ day22/classwork/cw.py
@@ -0,0 +1,17 @@
+names = ["გიორგი","მარიამი","თეონა","ლევანი","ნინო","ირაკლი","ანა","თათა","სამაზ","მარიანა"]
+
+for i in range(len(names)):
+    print(names[i])
+
+
+
+
+
+
+
+
+
+
+
+vegetables = ["viri", "კომბოსტო", "სვავი", "tagvit", "ბარდა", "bokveri"]
+print(vegetables)
