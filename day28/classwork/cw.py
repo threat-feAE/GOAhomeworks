f640d8bb7a2f35c3d81e0e28ac6f9042cf87efd3
threat-feAE <kopadzetemuri9@gmail.com>
2025-10-04 22:31:30 +0400

    1

---
+++ day28/classwork/cw.py
@@ -0,0 +1,22 @@
+k=["kopa","kola","lopa","tropa","gopa","rtopa","tripa","gorpa","tolpa","trimpa"]
+print(k[5:10])
+
+
+h=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+print(h[5:10])
+
+#orive pirvelia zevit
+
+
+
+
+t0=[i for i in range(1,21)]
+print("2) first 11 element:", t0[:11])
+
+
+
+
+
+z=[i for i in range(1,13)]
+num=int(input("write number (3-12); "))
+print("3) element indexit 2-dan",num,"mde",z[2:num])
