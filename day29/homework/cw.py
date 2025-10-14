77540f44dbb05a88174d02dfa47fb4bb02c0114f
threat-feAE <kopadzetemuri9@gmail.com>
2025-10-14 17:45:33 +0400

    1

---
+++ day29/homework/cw.py
@@ -0,0 +1,31 @@
+name="PythonProgramming"
+print(name[0:6])
+
+
+
+
+numbers = [10, 20, 30, 40, 50, 60, 70]
+print(numbers[-5:-2])
+
+
+
+al="HelloWorld"
+print(al[0:5])
+
+
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+print(letters[0],letters[2],letters[4])
+print(letters[-7],letters[-5],letters[-3])
+
+
+
+kola="Information"
+print(kola[2:7])
+
+
+
+kipa="abcdefghijklmno"
+print(kipa[0:4])
+print(kipa[9:16])
+print(kipa[5:10])
---
+++ gamocda/cw.py
@@ -0,0 +1,560 @@
+
+
+#2)ტერმინალში გამოიტანეთ 1 სტრინგი 1 ინტეჯერი 1 ფლოუთი 1 ბულეანი
+
+#3)შექმენი 4 ცვლადი snake_case ის გამოყენებით,1 ში შეინახე სტრინგი მეორეში ინტეჯერი მესამეში ბულეანი მეოთხეში ფლოუთი,დაპრინტე თითოეული ამთგანი(გამოიყენე ცვლადის სახელები)
+
+#4)შექმენი 2 ცვლადი სადაც შეინახავ ინტეჯერ ტიპის მონაცემებს,თქვენი დავალებაა რომ ამ ორ რიცხვზე მოახდინოთ ყველა მათემატიკური მოქმედება რაც ვისწავლეთ
+
+#5)შექმენი ორი ცვლადი შეინახე ორივეში სტრინგებიდ და შეკრიბე ეს ორი სტრინგი,ასევე კომენტარის სახით დაწერე რა ეწოდება ამ მოქმედებას(სტრინგების შეერთებას)
+
+
+
+
+
+
+
+
+
+
+
+
+           #  1  PIRVELIS PASUXEBI
+ #1)კომენტარის სახით ჩამოწერეთ რომელი მონაცემთა ტიპები ვისწავლეთ და თთეულს გვერდით მიუწერეთ 1 მაგალითი
+
+
+
+#string,"kai"
+# integer,15
+# float,14,2
+# boolean frue and false
+#print---print("goa")
+#input
+#output
+#snake case
+#>   <   ==   -  +   :  /   *     %     #   //      **
+#[],()
+#and
+#or
+#if--------if number>11:
+#          print("yochag")
+#elif------elif number<11:
+#           print("cudia")
+#else------else:
+#          print("dzaan cudia") 
+#float
+#type
+#slicing
+#for loop
+#while loop
+#dadebiti da uaryopiti index---(number[1:2])  an   (number[-6:-2])
+
+
+
+
+
+
+        #  2   MEOORES PASUXEBI
+
+u="hello"
+print(u)
+
+
+print(12)
+
+
+er=23.16
+print(er)
+
+
+
+tr=True
+y=False
+print(tr)
+print(y)
+
+
+
+
+        #  3  MESAMES PASUXEBI   
+
+name="temo"
+age=15
+biwia=True
+height=1.73 
+
+print(name,age,biwia,height)
+
+
+
+
+
+
+         #  4 MEOTXES PASUXEBI
+
+name1=137
+name2=145
+
+print(name1+name2)
+print(name1-name2)
+print(name1*name2)
+print(name1/name2)
+print(name2%name1)
+print(name2**name1)
+print(name2//name1)
+
+
+
+
+
+          #  5  MEXUTES PASUXEBI
+
+
+name3=("koka")
+name4=("kola")
+print(name3 + name4)
+
+
+
+
+          #   6   MEEKVSES PASUXEBI
+
+#6)კომენტარის სახით ახსენით თითოეული რას აკეთებს და მოიყვანე თითო მაგალით --> % , // , **
+
+#%---nashtis gamotvlas nishnavs  print(12%3)
+#//----mtel gayopas nishnavs (tu nashtiani gayopaa mtel ricxvs anu nashtis gareshe abrunebs)   print(10//6)
+#**------nishnavs xarisxshi ayvanas print(15**2)---anu 15 x 15
+
+
+
+
+
+         #  7   MESHVIDES PASUXEBI
+
+#7)მომხმარებელს შემოატანინე ორი რიცხვიდ ა მოახდინე აბსოლიტურად ყველა ამთემატიკური მოქმედება ამ ორ რიცხვზე --> + - * / // ** %
+
+
+name11=12
+name22=67
+
+print(name11+name22)
+print(name11-name22)
+print(name11*name22)
+print(name11/name22)
+print(name22%name11)
+print(name22**name11)
+print(name22//name11)
+
+
+
+
+
+
+                           #BONUS
+# ქვემოთ მოცემულ მაგალითებს გვერდით მიუწერეთ პასუხები(დააკომენტარეთ ყველა და კომენტარებად გქონდეთ მაგალითები:
+
+#      15 / 3 = 5
+#      20 / 4 = 5
+   
+
+#      15 // 10 = 1
+#      10 // 7 =  1
+   
+
+#      4 * 3 =  12
+#      40 * 3 =  120
+#      120 * 3 =  360
+
+#      4 ** 3 =   64
+#      10 ** 3 =  1000
+
+
+#      10 % 7 =   3
+#      3 % 2 =    1
+
+
+
+
+            #  8  MERVES PASUXEBI
+
+#8)კომენტარის სახით ახსენით თთოეული რას აკეთებს --> str() , int() , float(),მოიყვანე თითო მაგალითი სამივე მათგანზე
+
+#str()----es aris python punqcia romelic ricxvs booleans an sxva tips text-ad achvenebs(string-ad gardakmnis)------------
+
+#int()----es aris python punqcia romelic mnishvnelo9bas gadaakcevs mtel ricxvad-----------
+asaki="23"
+print("axalgazrda",int(asaki))
+
+
+#float()-----es aris python punqcia romelic ricxvs akcevs wiladad------------ 
+x2=5
+p3=float(x2)  
+print(p3)
+
+
+             #   9   MECXRES PASUXEBI
+
+
+#9)შექმენი 4 ცვლადი სადაც შეინახავ განსხვავებული ტიპის მონაცემებს,შენი დავალებაა რომ დაბეჭდო მათი ტიპები,გამოიყენე ნასწავლი ფუნქცია რომელიც იგებდა ფრჩხილებში გადაცემული ელემენტის ტიპს
+
+
+name99=12
+name94="kopa"
+name34=True
+name78=54.6
+
+
+print(type(name99))
+print(type(name94))
+print(type(name34))
+print(type(name78))
+
+
+
+
+
+                     #   10   MEATES PASUXEBI
+
+
+
+#10)მომხმარებელს შემოატანინე ორი რიცხვი,შენი დავალეაბაა ამ ორ რიცხვზე მოახდინოთ შედარების ოპერაციები(გამოიყენე ყველა შედარების ოპერატორი რაც ვისწავლეთ),
+    
+
+soup=231
+borshi=798
+print(borshi > soup)
+print(borshi < soup)
+print(borshi == soup)
+print(borshi >= soup)
+print(borshi <= soup)
+
+
+
+
+
+
+
+
+
+
+                # 11 METERTMETES PASUXEBI
+
+#11)კომენტარის სახით ახსენით რომელი ლოგიკური ოპერატორები ვისწავლეთ და რომლის გამოყენების დროს რა ხდება,მოიყვანე თითო მაგალითი 
+
+#logikuri gamnoiyeneba boolean gamotvlebshi and,or
+#or------- an erti wesmariti 
+#and-------da orive wsmariti unda iyos
+
+a=2
+b=3
+print(a or 1 <b)
+
+
+
+a2=2
+b2=3
+print(a2 and 34 >b2)
+
+
+
+
+                  #  12 METORMETES PASUXEBI
+
+#12)გვერდით მიუწერეთ პასუხები წერტილების მაგივრად
+#True and True -- ...true      |   True or True -- ...true
+#True and False -- ...false     |   True or False -- ...true
+#False and True -- ...false     |   False or True -- ...true
+#False and False -- ...false    |   False or False -- ...false
+
+
+
+
+
+                   #   13 MECAMETES PASUXEBI
+
+#13)გვერდით მიუწერეთ საბოლოო პასუხი წერტილების მაგივრად
+# 5 == 10 and 10 <= 10 -- ...false |   True or "giorgi" == "giorgi" -- ...true
+# True and False -- ...false        |   10 < 2 or "lasha" == "beqa" -- ...false
+# 10 > 3 and 5 == 3 -- ...false     |   2 >= 3 or 40 < 2 -- ...false
+# True and 200 <= 200 -- ...true    |   4 == 3 or 50 > 300 -- ...false
+
+
+
+
+
+                #  14 METOTXMETES PASUXEBI
+
+#14)ტერმინალში გამოიტანეთ თქვენი რიცხვები 5 იდან 200 მდე for loop
+
+for i in range(5,200):
+    print(i)
+
+
+
+
+
+                #  15 METXUTMETES PASUXEBI    
+
+#15)ტერმინალში გამოიტანეთ რიცვები 100 დან 150 მდე 5 ის სტეპის გამოტოვებით for loop
+
+
+
+
+
+
+
+
+
+                    #   16 METEKVSMETES PASUXEBI
+
+#16)ტერმინალში გამოიტანეთ თქვენი სახელი 10 ჯერ for loop
+for i in range(1,11):
+    print("temo")
+
+
+
+
+
+                   #   17   MECHVIDMETES PASUXEBI    
+
+#17)ტერმინალში გამოიტანეთ თქვენი რიცხვები 10 იდან 50 მდე while loop
+
+i=10
+while i <=49:
+    print(i)
+    i += 1
+
+
+
+
+                 #   18  METVRAMETES PASUXEBI
+
+#18)ტერმინალში გამოიტანეთ რიცვები 50 დან 150 მდე 10 ის სტეპის გამოტოვებით while loop
+
+i=50
+while i <=149:
+    print(i)
+    i += 1
+
+
+
+
+
+                #   19   MECXRAMETES PASUXEBI 
+
+#19)ტერმინალში გამოიტანეთ თქვენი სახელი 10 ჯერ while loop
+
+
+i=1
+while i <=10:
+    print("temo")
+    i += 1
+
+
+
+
+
+
+                #   20     MEOCES PASUXEBI  
+                
+#20) შექმენი ცვლადი სადაც შეინახავ სიას ,ამ სიაში მოათავსე 5 სტრინგ ტიპის მონაცემი და for loop ის დახმარებით გამოიტანე ყველა სიის ელემენტი
+
+tuy=["kopa","lopa","tropa","tripa","gopa"]
+for tuy in tuy :
+    print(tuy)
+
+
+
+
+
+
+
+
+
+                    #    21   OCDAMEERTES PASUXEBI
+
+#21) შექმენი ცვლადი სადაც შეინახავ სიას ,ამ სიაში მოათავსე 5 სტრინგ ტიპის მონაცემი დაwhile loop ის დახმარებით გამოიტანე ყველა სიის ელემენტი 
+
+tu=["kopa","lopa","tropa","tripa","gopa"]
+i=0
+while i <= 5:
+    print(tu)
+    i += 1
+
+
+ 
+
+                #      22    CODAMEORES PASUXEBI
+                
+#22)შექმენი სია --> ["გოგა" , "ირაკლი" , "საბა" , "გიორგი" , "ირმა" , "იამზე"],შენი დავალებაა ამ სიიდან გამოიტანო  საბა და იამზე როგორც დადებით ასევე უარყოფითი ინდექსების გამოყენებით
+
+names34=["გოგა" , "ირაკლი" , "საბა" , "გიორგი" , "ირმა" , "იამზე"]
+
+#saba
+
+print(names34[2])  
+print(names34[-4])
+
+#iamze
+
+print(names34[5])  
+print(names34[-1])
+
+
+
+
+                #      23  OCDAMESAMES PASUXEBI
+
+#23)შექმენი ცვლადი სადაც შეინახავ სტრინგს --> 'ჰიდროელექტროსადგური" , შენი დავალებაა რომ გამოიტანო მესამე და მერვე ინდექსზე მდგომი ასოები ტერმინალში
+
+kopex="ჰიდროელექტროსადგური"
+print(kopex[1:3])     #-14
+print(kopex[-18:-16])   #7
+
+
+
+
+
+
+
+
+
+                  #   24   OCDAMEOTXES PASUXEBI
+
+#24)შექმენი ცვლადი სადაც მომხმარებელს შემოატანინებ რიცხვს , შემდეგ --> თუ მომხმარებლის მიერ შემოტანილი რიცცხვი არის 0 ზე მეტი დაუპრინტე -->"more than 0" , თუ მომხმარებლის მიერ შემოტანილი რიცხვი არის 0 ზე ნაკლები დაუპრინტე --> "Less than 0",სხვა შემთხვევაშიდ აუპრინტე --> "Equal 0"
+
+hugo=int(input())
+
+if hugo > 0:
+    print("more than 0")
+elif hugo<0:
+    print("less than 0")
+else:
+    print("equal 0")
+
+
+
+
+
+
+
+                #    25     OCDAMEXUES PASUXEBI 
+
+#25)შექმენი ცვლადი სადაც შეინახავ სტადამიანების სახელებს(6 ცალი) სიას-->შემდეგ შეამოწმეთ --> თუ ამ სიაში მესამე ინდექსზე მდგომი ელემენტი არის "lasha" -->დაუპრინტე --> "You choose lasha" , თუ სიაში მყოფი ბოლო ინდექსზე მდგომი ელემენტი არის "giorgi" მაშინ დაუპრინტე -->"you choose giorgi" , სხვა შემთხვევაში დაუპრინტე --> "you choose other name"
+
+namesss=["temo","koba","toma","lasha","aleksi","gio"]
+
+if namesss[2] == "lasha":
+    print("you choosed lasha")
+elif namesss[5] == "gio":
+    print("you choosed gio")
+
+
+
+
+
+
+    
+                #    26    OCDAMEEKVSES PASUXEBI
+
+#26)შექმენი ცვლადი სადაც შეინახავ პაროლს მომხმარებელს შემოატანინე პაროლი სანამ პირობა არის ჭეშმარიტი(True) თუ მომხმარებლის პაროლი დაემთხვა შენს პაროლს: დაპრინტე --> გამოიცანი break სხვა შემთხვევაში: თავიდან გაუმეორე რომ შემოიყვანოს პაროლი
+
+
+kolip=int(input())
+jio=(234)
+
+if kolip == jio:
+    print("you guessed")
+else:
+    print("try again")
+
+
+               
+
+
+                
+
+                #  27  OCDAMESHVIDES PASUXEBI
+            
+#27)შექმენი ცვლადი და შეინახე სტრინგი --> " , შენი დავალებაა რომ ტერმინალში გამოიტანო სადგური ,გამოიყენე slicing, ასევე კიდევ გამოიტანე ელექტროსადგური აქაც გამოიყენე სლაისინგი
+
+tropaa="ჰიდროელექტროსადგური"
+
+print(tropaa[12:19])
+print(tropaa[5:19])
+
+
+
+
+
+
+
+
+
+                #  28    OCDAMERVES PASUXEBI
+
+#28)შექმენი ცვლადი და შეინახე სია --> ["გოგა" , "ირაკლი" , "საბა" , "გიორგი" , "ირმა" , "იამზე" , "სანი" , "თენგო" , "ლიზი" , "ნუცა" , "ქეთებვანი"] , შენი დავალებაა რომ ტერმინალში გამოიტანო ყველა სახელი საბადან სანიმდე,ასევე გამოიტანე სახელები გოგადან ირმა მდე გამოიყენე Slicing
+
+mimic=["გოგა" , "ირაკლი" , "საბა" , "გიორგი" , "ირმა" , "იამზე" , "სანი" , "თენგო" , "ლიზი" , "ნუცა" , "ქეთებვანი"]
+print(mimic[0:4])
+print(mimic[2:6])
+
+
+
+
+
+
+
+                  #   29    OCDAMECXRES PASUXEBI
+
+#29)შექმენი სია სადაც შეინახავთ ინტეჯერებს,შენი დავალებაა შეამოწმო,თუ სიაში მყოფი მენულე ინდეწსზე მდგომი ელემენტი მეტია სიაში მყოფ ბოლო ინდექსზე მდგომ ელემენტზე და(and) ასევე სიის პირველი ელემენტი მეტია 0 ზე დაუპრინტე --> good , თუ სიის პირველი ელემენტი მეტია სიის ბოლო ელემენტზე და(and) სიის ნოლო ელემენტი ნაკლებია 0 ზე დაუპრინტე --> bad , სხვა შემთხვევაში დაუპრინტე -->"get out"
+
+p=[312,34,62]
+
+
+
+if p[0] > p[2] and p[0] > 0:                                  
+    print("good")  
+elif p[0] < p[2] and p[2] < 0: 
+    print("bad")     
+else:
+    print("get out")                                                                                               
+
+
+
+
+
+
+
+                   #     30       OCDAMEATES PASUXEBI
+
+#30)შმომხმარებელს შემოატანინე ათწილადი,შემდეგ შეადარე,თუ მომხმარებლის მიერ შემოტანილი რიცხვი არის 10 ზე მეტი და 30 ზე ნაკლები მაინ დაუპრინტე -->You entered number between 10 and 30, თუ მომხმარებლის მიერ შემოტანილი რიცხვი არის 10 ზე ნაკლები და 0 ზე მეტი მაშნ დაუპრინტე --> You choose number between 0 and 10 , თუ მომხმარებლის მიერ შემოტანილი რიცხვი არის 0 ზე ნაკლები მაშინ დაუპრინტე you choose negative number , სხვა შემთხვევაში დაუპრინტე -->you choose zero(0)
+
+nummm=int(input())
+
+
+if nummm > 10 and nummm < 30:                                  
+    print("You entered number between 10 and 30")  
+elif nummm < 10 and nummm > 0: 
+    print("You choose number between 0 and 10")     
+else:
+    print("negative number")
+
+
+
+
+
+
+               #    31     OCDAMETERTMETES PASUXEBI
+
+#31)შექმენი სია სადაც შეინახავ სტრინგებს(ადამიანის სახელებს,რაც გინდათ და რამდენიც გინდათ),შემდეგ შეადარეთ თუ ამ სიაში მყოფი მე0 ინდექსზე მდგომი ელემენტი არის 'luka' ან(or) არის "saba" დაუპრინტე -->You choose luka or saba, თუ ამ სიაში მყოფი ბოლო ინდექსზე მყოფი ელემენტი არის "irma" ან(or) "iamze" მაშინ დაუპრინტე --> You choose irma or iamze,სხვა შემთხვევაში დაუპტინტე --> You choose other name
+
+
+kolia=["koko","loko","moko","doko"]
+
+if kolia[0] == "luka" or kolia[0] == "saba":
+    print("You choose luka or saba") 
+elif kolia[0] == "irma" or kolia[0] == "iamze":
+    print("You choose irma or iamze")
+else:
+    print("you chouse a nother name")
