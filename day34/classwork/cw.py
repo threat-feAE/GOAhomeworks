62b16fffe5d89be1dd8e6460c53467d57e8766f6
threat-feAE <kopadzetemuri9@gmail.com>
2025-10-25 22:59:03 +0400

    1

---
+++ day34/classwork/cw.py
@@ -0,0 +1,22 @@
+po = input("enter word: ")
+print(len(po))
+for i in range(len(po)):
+    print(po[i])
+
+
+
+
+
+
+s = ["მზე","კომპიუტერი","საღამო"]
+for i in range(len(s)):
+
+ if len(s[i])%2==0:
+    print(len(s[i]), "ლუწია")
+else:
+    print("kentia")
+
+
+
+
+
