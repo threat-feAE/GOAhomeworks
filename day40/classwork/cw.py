6f20a28839a04c7b44f610d94bea62075ba550e5
threat-feAE <kopadzetemuri9@gmail.com>
2025-11-18 20:54:55 +0400

    1

---
+++ day40/classwork/cw.py
@@ -0,0 +1,26 @@
+kop = [980, "saba", 231, "kote", "cico", True, "gio", 40.5]
+
+kop.append("ianvari")
+print(kop)
+
+
+
+kop = [980, "saba", 231, "kote", "cico", True, "gio", 40.5]
+
+kop.insert(2, "bati")
+print(kop)
+
+
+kop = [980, "saba", 231, "kote", "cico", True, "gio", 40.5]
+
+kop.pop(5)  
+print(kop)
+
+
+
+kop = [980, "saba", 231, "kote", "cico", True, "gio", 40.5]
+
+kop.remove(True)
+kop.remove("kote")
+
+print(kop)
