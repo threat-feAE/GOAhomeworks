b0228ebcc328d8e302149828572b9ce838eb480a
threat-feAE <kopadzetemuri9@gmail.com>
2025-11-25 19:39:17 +0400

    1

---
+++ day42/homework/cw.py
@@ -0,0 +1,54 @@
+# 1) 
+fruits = ["apple", "banana", "orange"]
+fruits.extend(["mango", "kiwi"])
+print(fruits) 
+
+# 2)
+numbers = [10, 20, 30]
+numbers.extend([40, 50])
+print(numbers)
+
+# 3) 
+names = ["saba", "gio", "nino"]
+names.reverse()
+print(names)  
+
+# 4) 
+nums = [1, 5, 3, 5, 5, 2]
+five = nums.count(5)
+print(five) 
+
+# 5) 
+letters = ["a","b","a","c"]
+a = letters.count("a")
+print(a)  
+
+# 6) 
+names = ["gio", "saba", "nino"]
+saba = names.index("saba")
+print(saba) 
+
+# 7)
+colors = ["red","green","blue"]
+blue = colors.index("blue")
+print(blue)  
+
+# 8) 
+nums = [1, 2, 3]
+nums.extend([7, 8, 9])
+print(nums)  
+
+# 9) 
+foods = ["pizza", "burger", "salad"]
+foods.reverse()
+print(foods) 
+
+# 10)
+cities = ["batumi", "tbilisi", "kutaisi"]
+tbilisi = cities.index("tbilisi")
+print(tbilisi) 
+
+# 11) 
+animals = ["cat","dog","cat","cow"]
+cat = animals.count("cat")
+print(cat)  
