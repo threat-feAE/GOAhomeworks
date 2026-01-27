4cbcb31a589fd2a9b86fb3dbb45b51494acd3177
threat-feAE <kopadzetemuri9@gmail.com>
2026-01-27 20:58:55 +0400

    1

---
+++ day58/homework/cw.py
@@ -0,0 +1,37 @@
+numbers = [5, 12, 7, 3, 20, 15]
+
+kopa = numbers[0]
+koba = numbers[0]
+
+for num in numbers:
+    if num > kopa:
+        koba = kopa
+        kopa = num
+    elif num > koba and num != kopa:
+        koba = num
+
+print("second biggest number  isss:", koba)
+
+
+
+
+
+
+
+sentence = input("შეიყვანეთ წინადადება: ")
+
+words = sentence.split()
+count = 0
+i = 0
+
+while i < len(words):
+    if len(words[i]) > 4:
+        count += 1
+    i += 1
+
+print("blablablaa:", count)
+
+
+
+
+
