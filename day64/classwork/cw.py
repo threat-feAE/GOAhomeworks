47e1e73b14b2d59a50db262e46030a9ef501e6d3
threat-feAE <kopadzetemuri9@gmail.com>
2026-02-07 23:15:31 +0400

    1

--- day64/classwork/cw.py
+++ day64/classwork/cw.py
@@ -4,3 +4,16 @@ def say_my_name():
 say_my_name()
 say_my_name()
 say_my_name()
+
+
+
+
+def check_even():
+    num = 17 
+
+    if num % 2 == 0:
+        return "this number is even"
+    else:
+        return "this number is odd"
+
+print(check_even())
